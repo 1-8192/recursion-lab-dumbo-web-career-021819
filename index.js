4714e3a5084c8f226e4b11a871e74891bf368690
1-8192 <allegranzia@gmail.com>
2019-07-09 10:06:47 -0400

    solved printString

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // Code your solution here!
+function printString(string) {
+    console.log(string[0])
+    if (string.length > 1) {
+        console.log(string[0])
+        let newString = string.substring(1, string.length)
+        printString(newString)
+    } else {
+    return true
+    }
+}
 
+function reverseString(string) {
+    let reversedString = string[string.length - 1]
+    if (string.length > 1) {
+        let newString = string.slice(0, -1)
+        reverseString(newString)
+    } else {
+        return reversedString
+    }
+}
